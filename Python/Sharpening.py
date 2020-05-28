cece8b0858479cfa918a3366b8420b935bf892f7
KJeanpol <ce.kalvarado@gmail.com>
2020-05-28 13:52:27 -0600

    GUI Fixed

--- Python/Sharpening.py
+++ Python/Sharpening.py
@@ -7,66 +7,9 @@ import numpy as np
 import cv2
 import PIL
 from PIL import ImageTk, Image
+import Prueba as Prueba
 
 
-
-def binToDecimal():  
-    file = open('res.txt', 'r')
-    flag=0
-    binario=""
-    lista=[]
-    while 1:       
-        # read by character 
-        char = file.read(1)           
-        if not char:  
-            break
-        binario=binario+char
-        if (flag<7):
-            flag=flag+1
-        else:
-            flag=0
-            try:
-                elemento=(int(binario,2))
-                lista.append(elemento)
-                binario=""
-            except:
-                binario=""
-                
-    file.close()
-    print(len(lista))
-    return lista
-    
-def ver(n,m):
-    newdata=binToDecimal()
-    newPic = Image.new('L', [n-2,m-2])
-    newPic.putdata(newdata)
-    newPic.save('test.bmp')
-    newPic.close()
-
-
-def holamundo():
-    print("Hola MUndo")
-
-def create_file(data, name):
-    f_out = open(name, 'w')
-    str_data = ''
-    for e in data:
-        if e <= 9:
-            str_data += '00'+str(e)
-        elif 10 <= e <= 99:
-            str_data += '0'+str(e)
-        else:
-            str_data += str(e)
-    f_out.write(str_data)
-    f_out.flush()
-    f_out.close()
-
-def runScript(n,m):
-
-    os.system("nasm -f elf64 conv.asm -o conv.o")
-    os.system("ld conv.o -o conv")
-    os.system("./conv "+str(n)+" "+str(m))
-    ver(n,m)
 
 class Root(Tk):
     def __init__(self):
@@ -85,16 +28,18 @@ class Root(Tk):
     def radioEvent(self):
         radSelected= self.radValues.get()
         if (radSelected==1):
-            self.createCanvasImage('foto.png')
-            holamundo()
-            print(1)
+            self.createCanvasImage(self.filename)
+
         elif (radSelected==2):
-            self.createCanvasImage('foto2.jpg')
-            print(2)
+            self.createCanvasImage('bw.png')
+ 
         elif (radSelected==3):
-            ver(364,681)
-            self.createCanvasImage('test.bmp')
-            print(3)
+            self.createCanvasImage('test.png')
+
+            
+        elif (radSelected==4):
+            self.createCanvasImage('test2.png')
+
         else:
             print ("Nada")
         
@@ -120,28 +65,21 @@ class Root(Tk):
         self.r1 = ttk.Radiobutton(self.labelFrame, text = "Normal", variable=self.radValues, value=1, command = self.radioEvent)
         self.r1.grid (column = 4, row =1)
         
-        self.r2 = ttk.Radiobutton(self.labelFrame, text = "Sharpening", variable=self.radValues,value=2, command = self.radioEvent)
-        self.r2.grid (column = 4, row =5)
+        self.r1 = ttk.Radiobutton(self.labelFrame, text = "Black and White", variable=self.radValues, value=2, command = self.radioEvent)
+        self.r1.grid (column = 4, row =5)
         
-        self.r3 = ttk.Radiobutton(self.labelFrame, text = "Over-Sharpening", variable=self.radValues,value=3, command = self.radioEvent)
-        self.r3.grid (column = 4, row =10)
+        self.r2 = ttk.Radiobutton(self.labelFrame, text = "Sharpening", variable=self.radValues,value=3, command = self.radioEvent)
+        self.r2.grid (column = 4, row =10)
+        
+        self.r3 = ttk.Radiobutton(self.labelFrame, text = "Over-Sharpening", variable=self.radValues,value=4, command = self.radioEvent)
+        self.r3.grid (column = 4, row =15)
         
         
     def fileDialog(self):
         self.filename = filedialog.askopenfilename (initialdir = "/", title = "Select a File",filetypes=(('png','*.png'),
                                                 ('bmp','*.bmp'),('jpeg','*.jpeg'),('Todos os arquivos','*.*')))
 
-        self.foto = Image.open(self.filename)
-        self.foto = self.foto.convert('L')
-        m,n=self.foto.size
-        print("m: "+str(m)+ " n: "+str(n) )
-        rows = self.foto.size[0]
-        cols = self.foto.size[0]
-        data = self.foto.getdata()
-        self.foto.close()
-        create_file(data, 'mybin.txt')
-        create_file([], 'res.txt')
-        runScript(n,m)
+        Prueba.hola(self.filename)
 
 
 if __name__ == '__main__':
